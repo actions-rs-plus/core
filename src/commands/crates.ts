f75d2c292f8e56acbfb951fcf5794be5a8b6c5c9
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-07-15 14:43:56 -0700

    chore: formatting

--- src/commands/crates.ts
+++ src/commands/crates.ts
@@ -4,9 +4,7 @@ import type { CratesIO } from "../schema";
 
 export async function resolveVersion(crate: string): Promise<string> {
     const url = `https://crates.io/api/v1/crates/${crate}`;
-    const client = new http.HttpClient(
-        "@actions-rs-plus (https://github.com/actions-rs-plus/)",
-    );
+    const client = new http.HttpClient("@actions-rs-plus (https://github.com/actions-rs-plus/)");
 
     const resp = await client.getJson<CratesIO>(url);
 
