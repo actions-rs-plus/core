b786990e0036a079c163d2b6051ec361060bc3c5
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-09-14 21:20:03 -0700

    fix: add eslint-plugin-perfectionist, make eslint report all errors & warnings on ALL files during build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,12 @@ import tsParser from "@typescript-eslint/parser";
 import love from "eslint-config-love";
 import importPlugin from "eslint-plugin-import-x";
 import nPlugin from "eslint-plugin-n";
+import perfectionist from "eslint-plugin-perfectionist";
 import prettier from "eslint-plugin-prettier/recommended";
 import promise from "eslint-plugin-promise";
 
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -161,6 +163,7 @@ export default tseslint.config(
             "import-x": importPlugin,
             n: nPlugin,
             promise,
+            perfectionist,
         },
         settings: {
             "import-x/resolver": {
@@ -226,10 +229,10 @@ export default tseslint.config(
                     selector: "enumMember",
                 },
             ],
-            "@typescript-eslint/no-empty-interface": ["error"],
             "@typescript-eslint/no-empty-object-type": ["error"],
             "@typescript-eslint/no-explicit-any": ["error", { fixToUnknown: true, ignoreRestArgs: false }],
             "@typescript-eslint/no-extraneous-class": ["error"],
+            "@typescript-eslint/no-magic-numbers": ["off"],
             "@typescript-eslint/no-shadow": ["error"],
             "@typescript-eslint/no-unused-expressions": [
                 "error",
@@ -257,7 +260,6 @@ export default tseslint.config(
             "@typescript-eslint/prefer-string-starts-ends-with": ["error"],
             "@typescript-eslint/promise-function-async": ["off"],
             "@typescript-eslint/require-await": ["error"],
-            "@typescript-eslint/sort-type-constituents": ["error"],
 
             "@typescript-eslint/unified-signatures": ["error"],
 
@@ -270,13 +272,22 @@ export default tseslint.config(
             "n/no-new-require": ["error"],
             "n/no-path-concat": ["error"],
             "n/process-exit-as-throw": ["error"],
+
+            "perfectionist/sort-intersection-types": ["error"],
+            "perfectionist/sort-union-types": ["error"],
+
             "promise/param-names": ["error"],
         },
     },
 
     {
         extends: [tseslint.configs.disableTypeChecked],
-        files: ["*.mjs"],
+        files: ["*.js", "*.mjs"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
         rules: {},
     },
     prettier,
