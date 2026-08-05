79cf1f17df646e43c3353953c47d16ebdc00ba2d
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-08-05 11:19:04 -0700

    test: auto-revert env stubs

--- src/tests/input.test.ts
+++ src/tests/input.test.ts
@@ -1,53 +1,53 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it, test, vi } from "vitest";
 
 import { getInput, getInputAsArray, getInputBool, getInputList } from "../input";
 
 describe("input", () => {
     describe("getInput", () => {
         it('env.INPUT_FOO | getInput("foo")', () => {
-            process.env["INPUT_FOO"] = "12345";
+            vi.stubEnv("INPUT_FOO", "12345");
             expect(getInput("foo")).toBe("12345");
         });
 
         it('env.INPUT_FOO-BAR | getInput("foo-bar")', () => {
-            process.env["INPUT_FOO-BAR"] = "12345";
+            vi.stubEnv("INPUT_FOO-BAR", "12345");
             expect(getInput("foo-bar")).toBe("12345");
         });
 
         it('env.INPUT_FOO_BAR | getInput("foo-bar")', () => {
-            process.env["INPUT_FOO_BAR"] = "12345";
+            vi.stubEnv("INPUT_FOO_BAR", "12345");
             expect(getInput("foo-bar")).toBe("12345");
         });
     });
 
     describe("getInputBool", () => {
         it('"true" == true', () => {
-            process.env["INPUT_VALUE"] = "true";
+            vi.stubEnv("INPUT_VALUE", "true");
             expect(getInputBool("value")).toBe(true);
         });
 
         it('"1" == true', () => {
-            process.env["INPUT_VALUE"] = "1";
+            vi.stubEnv("INPUT_VALUE", "1");
             expect(getInputBool("value")).toBe(true);
         });
 
         it('"True" == true', () => {
-            process.env["INPUT_VALUE"] = "True";
+            vi.stubEnv("INPUT_VALUE", "True");
             expect(getInputBool("value")).toBe(true);
         });
 
         it('"TRUE" == true', () => {
-            process.env["INPUT_VALUE"] = "TRUE";
+            vi.stubEnv("INPUT_VALUE", "TRUE");
             expect(getInputBool("value")).toBe(true);
         });
 
         it('"" == false', () => {
-            process.env["INPUT_VALUE"] = "";
+            vi.stubEnv("INPUT_VALUE", "");
             expect(getInputBool("value")).toBe(false);
         });
 
         it('"foobar" == false', () => {
-            process.env["INPUT_VALUE"] = "foobar";
+            vi.stubEnv("INPUT_VALUE", "foobar");
             expect(getInputBool("value")).toBe(false);
         });
     });
@@ -59,7 +59,7 @@ describe("input", () => {
             ["one,two,three", ["one", "two", "three"]],
             [",one , two , three", ["one", "two", "three"]],
         ])('getInputList("%s") == %s', (input, expected) => {
-            process.env["INPUT_VALUE"] = input;
+            vi.stubEnv("INPUT_VALUE", input);
             expect(getInputList("value")).toStrictEqual(expected);
         });
     });
@@ -71,7 +71,7 @@ describe("input", () => {
             ["one\ntwo\nthree", ["one", "two", "three"]],
             ["\none \n two \n three", ["one", "two", "three"]],
         ])('getInputAsArray("%s") == %s', (input, expected) => {
-            process.env["INPUT_VALUE"] = input;
+            vi.stubEnv("INPUT_VALUE", input);
             expect(getInputAsArray("value")).toStrictEqual(expected);
         });
     });
